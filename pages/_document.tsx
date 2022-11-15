46641a18cfa6eac1ac21d59b7c0977b6ba3615dd
Memo Chou <memochou1993@gmail.com>
2022-11-15 16:22:27 +0800

    Fix favicon

---
+++ pages/_document.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
