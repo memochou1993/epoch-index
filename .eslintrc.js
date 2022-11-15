8ff2ebd9d988e13c8f24d26a0a663d4df81c5c79
Memo Chou <memochou1993@gmail.com>
2022-11-15 13:49:36 +0800

    Fix lint

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    'next/core-web-vitals',
+    'airbnb',
+  ],
+  rules: {
+    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    'react/jsx-props-no-spreading': 'off',
+  },
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,23 @@
+import React from 'react';
 import pages from '../assets/pages.json';
 
-const Home = () => (
-  <>
-    <h1>Index of https://epoch.tw/</h1>
-    <ul>
-      {
-        pages.map((page) => (
-          <li key={page.link}>
-            <a href={page.link} target="_blank" rel="noreferrer">
-              { page.label }
-            </a>
-          </li>
-        ))
-      }
-    </ul>
-  </>
-);
+function Home() {
+  return (
+    <>
+      <h1>Index of https://epoch.tw/</h1>
+      <ul>
+        {
+          pages.map((page) => (
+            <li key={page.link}>
+              <a href={page.link} target="_blank" rel="noreferrer">
+                { page.label }
+              </a>
+            </li>
+          ))
+        }
+      </ul>
+    </>
+  );
+}
 
 export default Home;
